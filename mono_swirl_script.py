8aa273bc1350a22386ac82dcf819eeb48594400a
Cameron <155179899+camsexsmith@users.noreply.github.com>
2025-05-03 23:36:37 -0400

    c

--- mono_swirl_script.py
+++ mono_swirl_script.py
@@ -3,12 +3,12 @@ from functions import unit as u
 import math
 
 
-rh = u.in2m(0.060)
-rnw = u.in2m(0.20)
-R = u.in2m(0.20-0.060)
+rh = u.in2m(0.07)
+rnw = u.in2m(0.50)
+R = u.in2m(rnw-rh)
 
-mdot = 1
-rho = 1000
+mdot = 2
+rho = 1142
 
 n = 4
 
@@ -16,3 +16,5 @@ An = math.pi*rnw**2
 
 theta, phi_n, phi_ne, U_ne, U_n, V_ma_ne, V_ma_n, Vh, r_ma_ne, r_ma_n, K, Cd = S.swirl(R,rh,rnw,rho,mdot,n,An,True)
 
+dP = (mdot/(Cd*An))**2/(2*rho)
+print(dP/500/6895)
